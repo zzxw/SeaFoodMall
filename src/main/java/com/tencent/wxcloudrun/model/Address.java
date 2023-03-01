b94399c014daaf0143fc188b22b88caec9851e57
hunk <h446360065@163.com>
2023-03-02 01:41:52 +0800

    hunk.han 2023-03-01 修正了字段错误

--- src/main/java/com/tencent/wxcloudrun/model/Address.java
+++ src/main/java/com/tencent/wxcloudrun/model/Address.java
@@ -43,13 +43,44 @@ public class Address {
         this.provinceName = provinceName;
     }
 
+    public String getProvinceCode() {
+        return provinceCode;
+    }
 
-    public String getArea() {
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getDistrictName() {
         return districtName;
     }
 
-    public void setArea(String area) {
-        this.districtName = area;
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public String getDistrictCode() {
+        return districtCode;
+    }
+
+    public void setDistrictCode(String districtCode) {
+        this.districtCode = districtCode;
     }
 
     public String getDetail() {
@@ -90,46 +121,6 @@ public class Address {
 
     public void setIsDefault(int isDefault) {
         this.isDefault = isDefault;
-    }
-
-    public String getProvinceCode() {
-        return provinceCode;
-    }
-
-    public void setProvinceCode(String provinceCode) {
-        this.provinceCode = provinceCode;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
-
-    public String getCityCode() {
-        return cityCode;
-    }
-
-    public void setCityCode(String cityCode) {
-        this.cityCode = cityCode;
-    }
-
-    public String getDistrictName() {
-        return districtName;
-    }
-
-    public void setDistrictName(String districtName) {
-        this.districtName = districtName;
-    }
-
-    public String getDistrictCode() {
-        return districtCode;
-    }
-
-    public void setDistrictCode(String districtCode) {
-        this.districtCode = districtCode;
     }
 
     public String getAddress() {
